5aed6bee44b083485b4cd18276a1916b4a392e9c
David-Ildefonso <43185140+David-Ildefonso@users.noreply.github.com>
2018-09-13 21:02:04 +0200

    Add files via upload

---
+++ udemy_simple calculator.js
@@ -0,0 +1,18 @@
+//exercise 3
+//SUM
+var first = prompt("Insert first number");
+var second = prompt("Insert second number");
+var sum = Number(first) + Number(second);
+alert("The sum is " + sum);
+
+//SUB
+var sub = Number(first) - Number(second);
+alert("The subtraction value is " + sub);
+
+// TIMES
+var times = Number(first) * Number(second);
+alert("The multiplication value is " + times);
+
+// DIV
+var div = Number(first) / Number(second);
+alert("The division value is " + div);
